ac138863a70ede71487385b36ae83f841bb3729b
Aravindh <aravindhprabu2005@gmail.com>
2025-05-01 10:29:55 +0530

    routes

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./config/db");
-const authRoutes = require("./router/authRoutes");
+const authRoutes = require("./routes/authRoutes");
 
 dotenv.config();
 
